5b6295b6b8ae77db46d097c0482fd3c5588f354a
Jon Evans <jon@craftyjon.com>
2012-08-29 11:10:25 -0400

    clarity added to jsonserver.py

--- src/playground/jsonserver.py
+++ src/playground/jsonserver.py
@@ -14,10 +14,11 @@ store = None
 @route('/api/1.0/all_music')
 def all_music():
     lim = request.query.lim or '100'
-    lim = int(lim)
+    limit = int(lim)
     result = store.find(MediaItem, MediaItem.file_type == 0)
-    l = [pickle.loads(item.metadata) for item in result.order_by(MediaItem.file_path)[:lim]]
-    return {'all_music': l}
+    music_list = [pickle.loads(item.metadata) for item in result.order_by(MediaItem.file_path)[:limit]]
+    return {'all_music': music_list}
+
 
 if __name__ == "__main__":
     database = create_database("sqlite:" + DB_FILE)
